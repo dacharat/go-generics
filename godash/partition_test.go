1590f358fd8b530509dd378539649dff564433a1
dacharat <dacharat.p@ku.th>
2022-06-05 22:21:38 +0700

    :octocat: add partition test

--- godash/partition_test.go
+++ godash/partition_test.go
@@ -30,3 +30,24 @@ func (s *GodashTestSuite) TestPartitionInt() {
 		})
 	}
 }
+
+func (s *GodashTestSuite) TestPartitionPerson() {
+	testcases := []partitionTestCase[int]{
+		{
+			name:      "partition odd and even number",
+			input:     []int{1, 2, 3, 4, 5, 6, 7, 8},
+			fun:       func(i int) bool { return i%2 == 0 },
+			expected1: []int{2, 4, 6, 8},
+			expected2: []int{1, 3, 5, 7},
+		},
+	}
+
+	for _, tc := range testcases {
+		s.Run(tc.name, func() {
+			result1, result2 := godash.Partition(tc.input, tc.fun)
+
+			s.Equal(result1, tc.expected1)
+			s.Equal(result2, tc.expected2)
+		})
+	}
+}
